6f7b1fd96affe8b94cb64dab6caee22054b2617d
Manuel Barros Reyes <manuca@gmail.com>
2019-08-24 15:28:10 -0400

    Add Axios

--- app/javascript/packs/shortner.jsx
+++ app/javascript/packs/shortner.jsx
@@ -1,34 +1,43 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import axios from 'axios'
 
 class Shortner extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = { url: "" };
+    super(props)
+    this.state = { url: "", shortened_url: "" }
   }
 
   postUrl() {
-    console.log("Posting");
+    const {url} = this.state
+
+    axios.post('/urls', { url: url })
+      .then(response => console.log("Ok: " +  response))
+      .catch(error => console.log("Error: " +  error))
+  }
+
+  inputChanged(event) {
+    this.setState({url: event.target.value})
   }
 
   render() {
     return (
       <div>
-        <input />
+        <input value={this.state.url} onChange={(event) => this.inputChanged(event)} />
         <button onClick={() => this.postUrl()}>Shorten Me!</button>
       </div>
-    );
+    )
   }
 }
 
 Shortner.propTypes = {
   url: PropTypes.string
-};
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Shortner />,
     document.body.appendChild(document.createElement('div')),
   )
-});
+})
