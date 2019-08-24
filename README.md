3fbd4e8def74ddfa00e0837bb977080e9ad4701f
Manuel Barros Reyes <manuca@gmail.com>
2019-08-24 15:27:55 -0400

    Add basic create action

--- README.md
+++ README.md
@@ -5,4 +5,6 @@
 ## What didn't go well?
 
 ## If you had more time to work on this,
+  * Use a more elegant state handling library like Redux
+  * Add proper specs for the features
   * Put stats inside an authenticated environment
--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,8 +1,23 @@
 class UrlsController < ApplicationController
+  protect_from_forgery except: :create
+
   def new
     @url = Url.new
   end
 
   def create
+    url = Url.new(url_params)
+
+    if url.save
+      render json: { shortcode: url.shortcode }, status: :created
+    else
+      render json: { errors: {} }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def url_params
+    params.permit(:url)
   end
 end
