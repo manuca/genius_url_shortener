8637de9178cf5cc8a0e09fd8a0876097219bf675
Manuel Barros Reyes <manuca@gmail.com>
2019-08-26 18:02:19 -0300

    Remove propTypes

--- app/javascript/packs/shortner.jsx
+++ app/javascript/packs/shortner.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
 import axios from 'axios'
 
 const Errors = ({errors}) => {
@@ -93,10 +92,6 @@ class Shortner extends React.Component {
       </div>
     )
   }
-}
-
-Shortner.propTypes = {
-  url: PropTypes.string
 }
 
 document.addEventListener('DOMContentLoaded', () => {
