2d2ea95bc97cffa1d068adef25fe5b0b313febe4
Manuel Barros Reyes <manuca@gmail.com>
2019-09-07 09:22:54 -0300

    Add link to shortcode

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :urls, only: [:create, :index]
   resources :visits, only: [:index]
-  get '*shortcode' => 'redirects#show'
+  get '*shortcode' => 'redirects#show', as: :redirects
   root to: 'urls#new'
 end
